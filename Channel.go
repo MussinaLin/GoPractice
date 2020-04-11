47fe7c56f10528b77d08ce106e33d817bb1021de
Mussina <mussinalin@gmail.com>
2020-04-11 22:48:59 +0800

    feat: only write and only read channel

--- Channel.go
+++ Channel.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main(){
 	c := make(chan bool)
@@ -9,6 +11,25 @@ func main(){
 		<- c
 
 	}()
-
 	c <- true
+
+	ch := make(chan int)
+
+	// only read channel
+	go func(ch <- chan int) {
+		for v := range ch{
+			fmt.Println(v)
+		}
+
+	}(ch)
+
+	// only write channel
+	go func(ch chan <- int) {
+		ch <- 100
+		ch <- 101
+		ch <- 102
+	}(ch)
+	//time.Sleep(1 * time.Second)
+	ch <- 104
+	ch <- 105
 }
